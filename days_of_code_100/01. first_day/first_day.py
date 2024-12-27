2c2fdc0ff9da16cbd0bde34462849b7bad68b0b9
naodatacs <naodatacs@gmail.com>
2024-12-26 22:12:16 -0500

    Modifications

--- days_of_code_100/01. first_day/first_day.py
+++ days_of_code_100/01. first_day/first_day.py
@@ -1,7 +1,7 @@
-#project band name generator
-print("Welcome to new Band Name Generator") #Print the title
-print("What's the name of the city you grew up in?") #Print the question abou your City 
-city_name = str(input()) #write your city name
-print("What's you pet's name?") #Print the question abou your pet's name 
-pet_name = str(input()) #write your pet name
+# project band name generator
+print("Welcome to new Band Name Generator") # Print the title
+print("What's the name of the city you grew up in?") # Print the question abou your City 
+city_name = str(input()) # write your city name
+print("What's you pet's name?") # Print the question abou your pet's name
+pet_name = str(input()) # write your pet name
 print(f"Your band name could be {city_name} {pet_name}") #Print in f-strings
