355572091c0352610d2abc4e4068e4fb1d20fe83
naodata-CS <naodatacs@gmail.com>
2025-03-06 01:31:52 -0500

    "New location"

--- CPP/Learn_CPP_programming_beginner_to_advanced_Deep_dive_in_CPP/01.Essential_fundamentals/practice_2.cpp
+++ CPP/Learn_CPP_programming_beginner_to_advanced_Deep_dive_in_CPP/01.Essential_fundamentals/practice_2.cpp
@@ -5,7 +5,7 @@ int main() {
     std::cout << "Enter the dolars you want to exchange: " << std::endl;
     std::cin >> dolar;
     euro = dolar * 0.95;
-    yen = dolar * 149.42;
+    japanse = dolar * 149.42;
     std::cout << "Tu cantidad de euros es " << euro << "$ y de yenes es " << yen
               << "$" << std::endl;
     return 0;
